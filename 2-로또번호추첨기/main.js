af2f6e81ad4d0b3b9a374c7095007d5e14c909e0
lee suhan <suhanee55@gmail.com>
2023-03-29 17:00:17 +0900

    frist commit

---
+++ 2-로또번호추첨기/main.js
@@ -0,0 +1,33 @@
+const clickBtn = document.querySelector(".confirm");
+
+clickBtn,addEventListener("click", () => {
+  const randomNums = document.querySelectorAll(".randomNum");
+  const plusNum = document.querySelector(".plusNum");
+
+  const numbers = [];
+
+  while (numbers.length < 6) {
+    const num = Math.floor(Math.random() * 45) + 1;
+    if (!numbers.includes(num)) {
+      numbers.push(num);
+    }
+  }
+  numbers.sort((a, b) => a - b);
+
+  let i = 0;
+  const interval = setInterval(() => {
+    randomNums[i].textContent = numbers[i];
+    i++;
+    if (i >= 6) {
+      clearInterval(interval);
+      setTimeout(() => {
+        let randomPlusNum;
+        randomPlusNum = Math.floor(Math.random() * 45) + 1;
+        while (numbers.includes(randomPlusNum));
+
+        plusNum.textContent = randomPlusNum;
+        console.log(numbers, randomPlusNum);
+      }, 1000);
+    }
+  }, 1000);
+});
