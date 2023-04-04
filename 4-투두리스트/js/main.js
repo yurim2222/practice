6ad6fa3b578dabd18b072ebba4d4a45e185e0a62
lee suhan <suhanee55@gmail.com>
2023-04-04 12:25:30 +0900

    frist commit

---
+++ 4-투두리스트/js/main.js
@@ -0,0 +1,7 @@
+const addNew =document.querySelector(".addnewBtn")
+const flowdialog =document.querySelector(".favDialog")
+
+addNew.addEventListener("click", ()=>{
+    flowdialog.showModal();
+});
+
