7b26665057679047f56210b3d2ff6d4bc81d4432
lee suhan <suhanee55@gmail.com>
2023-04-05 17:34:06 +0900

    e.target

--- 4-투두리스트/js/main.js
+++ 4-투두리스트/js/main.js
@@ -15,39 +15,7 @@ let todoInfo = [];
 const confirmBtn = document.getElementById("confirmBtn");
 const countNumber = document.querySelector(".countNumber");
 
-// delete 함수 정의
-function deleteTask(taskContainer, newTodo) {
-  // 한번 클릭시 checkBox 배경색 빨간색으로 변경하기
-  const checkBox = taskContainer.querySelector("div");
-  checkBox.style.backgroundColor = "red";
 
-  // 두번째 클릭시 해당 div의 부모 요소에서 삭제하기
-  taskContainer.parentNode.removeChild(taskContainer);
-
-  // todoInfo 배열에서 해당 요소 삭제하기
-  const index = todoInfo.indexOf(newTodo);
-  if (index > -1) {
-    todoInfo.splice(index, 1);
-  }
-
-  // count 갱신하기
-  countNumber.textContent = todoInfo.length;
-};
-
-// hidden 메시지 보여주는 함수 정의
-function toggleHiddenContent(taskContainer) {
-  const taskContent = taskContainer.querySelector("p");
-  const taskTime = taskContainer.querySelector("time");
-  if (taskContent.style.display === "none" && taskTime.style.display === "none") {
-    taskContent.style.display = "flex";
-    taskTime.style.display = "flex";
-  } else {
-    taskContent.style.display = "none";
-    taskTime.style.display = "none";
-  }
-};
-
-//confirmBtn 클릭 이벤트 리스너 
 confirmBtn.addEventListener("click", () => {
   const title = todoTitle.value;
   const content = todoContents.value;
@@ -82,29 +50,78 @@ confirmBtn.addEventListener("click", () => {
   // div 요소 생성해서 내용 담기  
   const main = document.querySelector(".main");
   const taskContainer = document.createElement("div");
-  taskContainer.classList.add("task-container");
-  taskContainer.innerHTML = `
-  <div style="width: 25px; height: 25px; border: 1px solid #bbe0f8;"></div>
-  <h3>${title}</h3>
-  <button>완료</button>
-  <span>목표시간${time}</span>
-`;
-  const taskContent = document.createElement("p");
-  taskContent.textContent = content;
-  const createtime = document.createElement("time");
   const now = new Date();
   const hour = now.getHours().toString().padStart(2, '0');
   const minute = `${now.getMinutes().toString().padStart(2, '0')}`;
-  createtime.textContent = `만든 시간 ${hour}:${minute}`;
+  taskContainer.classList.add("task-container");
+  taskContainer.innerHTML = `
+  <div class="checkbox"; style="width: 25px; height: 25px; border: 1px solid #bbe0f8;"></div>
+  <h3>${title}</h3>
+  <p>${content}</p>
+  <button>완료</button>
+  <span>목표시간${time}</span>
+  <time>만든 시간${hour}:${minute}</time>
+`;
+  const taskContent = document.createElement("p");
+  taskContent.textContent = content;
   
-
-
   
   taskContainer.appendChild(taskContent);
-  taskContainer.appendChild(createtime);
   main.appendChild(taskContainer);
-  console.log(todoInfo);
+    
 });
+
+
+
+// 완료"버튼 클릭시 해당 div 삭제하기
+let isDeleted = false;
+const main = document.querySelector("main");
+main.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    // 한번 클릭시 checkBox 배경색 빨간색으로 변경하기
+    const checkBox = e.target.parentNode.querySelector(".checkbox");
+    checkBox.style.backgroundColor = "red";
+
+    const deleted = e.target.parentNode.parentNode;
+
+    if (isDeleted) {
+      // 두번째 클릭시 해당 div의 부모 요소에서 삭제하기
+      deleted.parentNode.removeChild(deleted);
+
+      // todoInfo 배열에서 해당 요소 삭제하기
+      const index = todoInfo.indexOf(newTodo);
+      if (index > -1) {
+        todoInfo.splice(index, 1);
+      }
+
+      // count 갱신하기
+      countNumber.textContent = todoInfo.length;
+    } else {
+      isDeleted = true;
+    }
+  }
+});
+
+
+
+
+  
+  //taskTitle클릭시 숨겨진 내용 출력 
+    const taskTitle = document.querySelector("main");
+    taskTitle.addEventListener("click", (e) => {
+    if (e.target.nodeName === "H3"){
+      const taskContainer = e.target.parentNode;
+      const taskContent = taskContainer.querySelector("p");
+      const taskTime = taskContainer.querySelector("time");
+      if (taskContent.style.display === "none" && taskTime.style.display === "none") {
+        taskContent.style.display = "flex";
+        taskTime.style.display = "flex";
+      } else {
+        taskContent.style.display = "none";
+        taskTime.style.display = "none";
+      }
+    }
+  });
 
 
   
